174dd279ff4672d468d5acac6bfe4c3b7be51f1c
dianhsu <xudian.cn@gmail.com>
2020-08-14 15:00:44 +0800

    判题机添加SPJ编译

--- ddlcw/__init__.py
+++ ddlcw/__init__.py
@@ -9,6 +9,7 @@ import psutil
 from ddlcw import config
 from ddlcw import runner
 from ddlcw.exceptions import CompileError, JudgeError
+from ddlcw import languages
 
 
 def _run(instance, test_case):
@@ -34,16 +35,68 @@ class Runner:
         self._src_path = os.path.join(self._runner_path, self._compile_config['src_name'])
         with open(self._src_path, 'w') as f:
             f.write(code)
+
         self._exe_path = os.path.join(self._runner_path, self._compile_config["exe_name"])
         if not os.path.exists(self._runner_path):
             os.makedirs(self._runner_path)
             os.chown(self._runner_path, config.RUN_USER_UID, config.RUN_GROUP_GID)
         self._compiler_out = os.path.join(self._runner_path, "compiler.out")
         self._compiler_log = os.path.join(self._runner_path, "compiler.log")
-
+        self._spj = False
+        self._spj_code = ''
+        self._spj_src_path = ''
+        self._spj_exe_path = ''
+        self._spj_version = 'ver1'
         self._run_config = language_config['run']
         self._run_log = os.path.join(self._runner_path, "run.log")
         self._pool = Pool(processes=psutil.cpu_count())
+        if self._manifest['spj'] is True:
+            self._spj = True
+            self._spj_code = self._manifest['spj_code']
+            self._spj_src_path = os.path.join(self._runner_path,
+                                              languages.c_lang_spj_compile['src_path']).format(
+                spj_version=self._spj_version)
+            self._spj_exe_path = os.path.join(self._runner_path,
+                                              languages.c_lang_spj_compile['exe_path']).format(
+                spj_version=self._spj_version)
+            with open(self._spj_src_path, 'w') as f:
+                f.write(self._spj_code)
+
+    def _compile_spj(self):
+        compile_config = languages.c_lang_spj_compile
+        command = compile_config['compile_command']
+        command = command.format(src_path=self._spj_src_path, exe_path=self._spj_exe_path)
+        _command = command.split(' ')
+        os.chdir(self._runner_path)
+        env = 'PATH=' + os.getenv('PATH')
+        spj_compile_result = runner.run(max_cpu_time=compile_config['max_cpu_time'],
+                                        max_real_time=compile_config['max_real_time'],
+                                        max_memory=compile_config['max_memory'],
+                                        max_stack=128 * 1024 * 1024,
+                                        max_output_size=20 * 1024 * 1024,
+                                        max_process_number=config.UNLIMITED,
+                                        exe_path=_command[0],
+                                        input_path=self._spj_src_path,
+                                        output_path=self._compiler_out,
+                                        error_path=self._compiler_out,
+                                        args=_command[1::],
+                                        env=env,
+                                        log_path=self._compiler_log,
+                                        seccomp_rule_name=None,
+                                        uid=config.RUN_USER_UID,
+                                        gid=config.RUN_GROUP_GID)
+        if spj_compile_result["result"] != config.RESULT_SUCCESS:
+            if os.path.exists(self._compiler_out):
+                with open(self._compiler_out, encoding="utf-8") as f:
+                    error = f.read().strip()
+                    os.remove(self._compiler_out)
+                    if error:
+                        raise CompileError(error)
+            raise CompileError("Compiler runtime error, info: %s" % json.dumps(spj_compile_result))
+        else:
+            if os.path.exists(self._compiler_out):
+                os.remove(self._compiler_out)
+            return self._spj_exe_path
 
     def compile(self):
         compile_config = self._language_config['compile']
