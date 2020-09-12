95e62cac4ae31bb329b4e1f4121e40e8b851ee6a
dianhsu <xudian.cn@gmail.com>
2020-09-12 18:54:27 +0800

    add kotlin language

---
+++ test/1/main.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+fun main(){
+    val sc = Scanner(System.`in`);
+    while(sc.hasNext()){
+        val a = sc.nextInt()
+        val b = sc.nextInt()
+        println(a+b)
+    }
+}
